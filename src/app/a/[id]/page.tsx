86f61f3569a5d3598f2df79c4945055a432477de
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-02-10 21:59:59 +0100

    Add a confirmation modal for the restart button

--- src/app/a/[id]/page.tsx
+++ src/app/a/[id]/page.tsx
@@ -8,6 +8,8 @@ import { useQuery } from "@/usingSession";
 import ExplainExercise from "@/components/exercises/ExplainExercise";
 import QuizExercise from "@/components/exercises/QuizExercise";
 import ReadingExercise from "@/components/exercises/ReadingExercise";
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
 export default function Page({ params }: { params: { id: string } }) {
   const attemptId = params.id as Id<"attempts">;
@@ -21,7 +23,7 @@ export default function Page({ params }: { params: { id: string } }) {
   return (
     <div className="p-6">
       <div className="max-w-xl mx-auto">
-        <header className="fixed h-16 top-0 left-0 w-full bg-white bg-opacity-90 backdrop-blur-lg p-4 shadow-lg flex items-center justify-center z-30">
+        <header className="fixed h-16 top-0 left-0 w-full bg-white bg-opacity-90 backdrop-blur-lg p-4 shadow-lg flex items-center justify-center z-5">
           <Link
             href="/"
             title="Back"
@@ -35,13 +37,7 @@ export default function Page({ params }: { params: { id: string } }) {
           </h1>
 
           {metadata.status === "exercise" && metadata.text === null && (
-            <Link
-              href={`/a/new?exerciseId=${metadata.exerciseId}`}
-              title="Back"
-              className="absolute top-0 right-0 w-16 h-16 flex items-center justify-center"
-            >
-              <ArrowPathIcon className="w-6 h-6" />
-            </Link>
+            <RestartButton exerciseId={metadata.exerciseId} />
           )}
         </header>
 
@@ -70,5 +66,86 @@ export default function Page({ params }: { params: { id: string } }) {
         )}
       </div>
     </div>
+  );
+}
+
+function RestartButton({ exerciseId }: { exerciseId: Id<"exercises"> }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        title="Restart the conversation"
+        className="absolute top-0 right-0 w-16 h-16 flex items-center justify-center"
+        onClick={() => setIsModalOpen(true)}
+      >
+        <ArrowPathIcon className="w-6 h-6" />
+      </button>
+
+      <Transition appear show={isModalOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => setIsModalOpen(false)}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Restart the exercise?
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      If you’re experiencing issues, you can restart the
+                      exercise and try again.
+                    </p>
+                  </div>
+
+                  <div className="mt-4 flex gap-2 justify-end">
+                    <button
+                      className="inline-flex justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-gray-600 hover:bg-gray-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => setIsModalOpen(false)}
+                    >
+                      Cancel
+                    </button>
+                    <Link
+                      href={`/a/new?exerciseId=${exerciseId}`}
+                      className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => setIsModalOpen(false)}
+                    >
+                      Restart
+                    </Link>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
   );
 }
