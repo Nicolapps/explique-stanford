5bade77187d95c211d617253365a64ab2c56e8dd
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-02-11 14:27:20 +0100

    Improve the message composition component

--- src/components/exercises/ExplainExercise.tsx
+++ src/components/exercises/ExplainExercise.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import {
@@ -108,12 +108,7 @@ export default function ExplainExercise({
         ))}
       </div>
 
-      {!isCompleted && (
-        <>
-          <div className="h-20"></div>
-          <NewMessage attemptId={attemptId} />
-        </>
-      )}
+      {!isCompleted && <NewMessage attemptId={attemptId} />}
     </>
   );
 }
@@ -122,30 +117,72 @@ function NewMessage({ attemptId }: { attemptId: Id<"attempts"> }) {
   const sendMessage = useMutation(api.chat.sendMessage);
   const [message, setMessage] = useState("");
 
+  function autoResizeTextarea() {
+    if (!textareaRef.current || !paddingRef.current) return;
+
+    const isDocumentScrolledToBottom =
+      window.innerHeight + window.scrollY >= document.body.scrollHeight;
+
+    textareaRef.current.style.height = "0";
+    const newHeight = Math.min(500, textareaRef.current.scrollHeight) + "px";
+    textareaRef.current.style.height = newHeight;
+    paddingRef.current.style.height = newHeight;
+
+    if (isDocumentScrolledToBottom) {
+      window.scrollTo({ top: document.body.scrollHeight });
+    }
+  }
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  useEffect(() => {
+    window.addEventListener("resize", autoResizeTextarea);
+    return () => window.removeEventListener("resize", autoResizeTextarea);
+  });
+
+  const paddingRef = useRef<HTMLDivElement>(null);
+
+  function send() {
+    const messageSent = message.trim();
+    if (!messageSent) return;
+    sendMessage({ attemptId, message: messageSent });
+    setMessage("");
+  }
+
   return (
-    <form
-      className="fixed bottom-2 left-2 h-14 flex w-[calc(100%-1rem)] shadow-xl rounded-xl"
-      onSubmit={(e) => {
-        e.preventDefault();
-        const messageSent = message.trim();
-        if (!messageSent) return;
-        sendMessage({ attemptId, message: messageSent });
-        setMessage("");
-      }}
-    >
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        className="w-full bg-transparent text-lg pl-5 pr-16 rounded-xl bg-white"
-      />
-      <button
-        className="bg-purple-600 text-white absolute right-2 top-[50%] translate-y-[-50%] w-12 h-12 rounded-full flex items-center justify-center shadow-md"
-        type="submit"
-        title="Send"
+    <>
+      <div className="box-content h-[60px] pt-4" ref={paddingRef} />
+      <form
+        className="fixed bottom-2 left-2 flex w-[calc(100%-1rem)] shadow-xl rounded-xl"
+        onSubmit={(e) => {
+          e.preventDefault();
+          send();
+        }}
       >
-        <PaperAirplaneIcon className="w-6 h-6" />
-      </button>
-    </form>
+        <textarea
+          ref={textareaRef}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="w-full bg-transparent text-lg px-4 rounded-xl resize-none bg-white py-4 h-[60px] pr-16"
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !e.shiftKey && !message.includes("\n")) {
+              e.preventDefault();
+              send();
+            }
+          }}
+          onInput={(e) => {
+            autoResizeTextarea();
+          }}
+        />
+        <div className="flex px-2 items-center right-0 inset-y-0 absolute">
+          <button
+            className="bg-purple-600 text-white w-12 h-12 rounded-full flex items-center justify-center shadow-md"
+            type="submit"
+            title="Send"
+          >
+            <PaperAirplaneIcon className="w-6 h-6" />
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
