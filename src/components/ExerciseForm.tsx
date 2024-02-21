08e2807477cbb81c9a2ee816473449443f64e7d2
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-02-21 17:27:18 +0100

    Fix incorrect models

--- src/components/ExerciseForm.tsx
+++ src/components/ExerciseForm.tsx
@@ -34,18 +34,6 @@ export type State = {
   completionFunctionDescription: string;
 };
 
-const OPENAI_MODELS = [
-  "gpt-4",
-  "gpt-4-turbo-preview",
-  "gpt-4-0125-preview",
-  "gpt-4-1106-preview",
-  "gpt-4-0613",
-  "gpt-3.5-turbo",
-  "gpt-3.5-turbo-0125",
-  "gpt-3.5-turbo-1106",
-  "gpt-3.5-turbo-0613",
-] as const;
-
 export function toConvexState(state: State) {
   return {
     name: state.name,
@@ -254,7 +242,17 @@ export default function ExerciseForm({
           label="Model"
           value={model}
           onChange={setModel}
-          values={OPENAI_MODELS.map((value) => ({ value, label: value }))}
+          values={[
+            "gpt-4",
+            "gpt-4-turbo-preview",
+            "gpt-4-0125-preview",
+            "gpt-4-1106-preview",
+            "gpt-4-0613",
+            "gpt-3.5-turbo",
+            "gpt-3.5-turbo-0125",
+            "gpt-3.5-turbo-1106",
+            "gpt-3.5-turbo-0613",
+          ].map((value) => ({ value, label: value }))}
           hint={
             <>
               More information about the models can be found in the{" "}
@@ -322,7 +320,21 @@ export default function ExerciseForm({
                   label="Model"
                   value={feedback.model}
                   onChange={(m) => setFeedback({ ...feedback, model: m })}
-                  values={OPENAI_MODELS.map((value) => ({
+                  values={[
+                    "gpt-4-1106-preview",
+                    "gpt-4",
+                    "gpt-4-0314",
+                    "gpt-4-0613",
+                    "gpt-4-32k",
+                    "gpt-4-32k-0314",
+                    "gpt-4-32k-0613",
+                    "gpt-3.5-turbo",
+                    "gpt-3.5-turbo-16k",
+                    "gpt-3.5-turbo-0301",
+                    "gpt-3.5-turbo-0613",
+                    "gpt-3.5-turbo-1106",
+                    "gpt-3.5-turbo-16k-0613",
+                  ].map((value) => ({
                     value,
                     label: value,
                   }))}
