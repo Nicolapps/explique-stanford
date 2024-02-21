c6cc7e2f195ae3ab6e46569a1b08ba01af9fef69
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-02-21 17:29:14 +0100

    Fix transcript generation

--- convex/chat.ts
+++ convex/chat.ts
@@ -307,31 +307,31 @@ export const startFeedback = internalAction({
     prompt: v.string(),
   },
   handler: async (ctx, { attemptId, feedbackMessageId, model, prompt }) => {
-    const transcript = await ctx.runQuery(internal.chat.generateTranscript, {
-      attemptId,
-    });
-
-    const validModels = [
-      "gpt-4-1106-preview",
-      "gpt-4-vision-preview",
-      "gpt-4",
-      "gpt-4-0314",
-      "gpt-4-0613",
-      "gpt-4-32k",
-      "gpt-4-32k-0314",
-      "gpt-4-32k-0613",
-      "gpt-3.5-turbo",
-      "gpt-3.5-turbo-16k",
-      "gpt-3.5-turbo-0301",
-      "gpt-3.5-turbo-0613",
-      "gpt-3.5-turbo-1106",
-      "gpt-3.5-turbo-16k-0613",
-    ] as const;
-    if (!validModels.includes(model as any)) {
-      throw new Error(`Invalid model ${model}`);
-    }
-
     try {
+      const validModels = [
+        "gpt-4-1106-preview",
+        "gpt-4-vision-preview",
+        "gpt-4",
+        "gpt-4-0314",
+        "gpt-4-0613",
+        "gpt-4-32k",
+        "gpt-4-32k-0314",
+        "gpt-4-32k-0613",
+        "gpt-3.5-turbo",
+        "gpt-3.5-turbo-16k",
+        "gpt-3.5-turbo-0301",
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-1106",
+        "gpt-3.5-turbo-16k-0613",
+      ] as const;
+      if (!validModels.includes(model as any)) {
+        throw new Error(`Invalid model ${model}`);
+      }
+
+      const transcript = await ctx.runQuery(internal.chat.generateTranscript, {
+        attemptId,
+      });
+
       const openai = new OpenAI();
       const response = await openai.chat.completions.create({
         model,
@@ -342,7 +342,7 @@ export const startFeedback = internalAction({
           },
           {
             role: "user",
-            content: "She no went to the market.",
+            content: transcript,
           },
         ],
         temperature: 0.7,
