e7c03a1127d29c9e8b2cf12db8c5e2ce1a3da783
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-03-17 21:32:45 +0100

    Extract validateAdminRequest

---
+++ src/util/admin.ts
@@ -0,0 +1,26 @@
+import * as jsrsasign from "jsrsasign";
+
+export function validateAdminRequest(req: Request): Response | null {
+  const publicKey = process.env.ADMIN_API_PUBLIC_KEY;
+  if (!publicKey) {
+    console.error("Configuration issue: ADMIN_API_PUBLIC_KEY is not set");
+    return Response.json({ error: "Configuration issue" }, { status: 500 });
+  }
+
+  const token = req.headers.get("Authorization");
+  if (!token) {
+    return Response.json({ error: "No token provided" }, { status: 401 });
+  }
+
+  const jwt = token.replace(/^Bearer /, "");
+  const verify = jsrsasign.KJUR.jws.JWS.verifyJWT(jwt, publicKey, {
+    alg: ["RS256"],
+    iss: ["https://cs250.epfl.ch"],
+    aud: ["admin"],
+  });
+  if (!verify) {
+    return Response.json({ error: "Invalid token" }, { status: 401 });
+  }
+
+  return null;
+}
