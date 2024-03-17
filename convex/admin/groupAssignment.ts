2105d4f56cd09f3e29f6aac7633833472647dce1
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-03-17 21:41:15 +0100

    Remove unused import

--- convex/admin/groupAssignment.ts
+++ convex/admin/groupAssignment.ts
@@ -1,4 +1,4 @@
-import { ConvexError, v } from "convex/values";
+import { v } from "convex/values";
 import { internalMutation } from "../_generated/server";
 import Chance from "chance";
 import { queryWithAuth } from "../withAuth";
