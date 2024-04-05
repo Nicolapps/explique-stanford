1819f44ecc48e2fda21a236eb89a671d0afaa8b0
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-04-05 17:51:51 +0200

    Revert "Temporarily log errors"
    This reverts commit 63be9aebd4cabf4fd0ab8f2d7eb04949f0ee147c.

--- src/app/api/admin/migrate/route.ts
+++ src/app/api/admin/migrate/route.ts
@@ -1,28 +1,25 @@
 export async function POST(req: Request) {
-  try {
-    const path = await import("path");
-    const { migrate } = await import("drizzle-orm/better-sqlite3/migrator");
-    const { db } = await import("../../../../../drizzle/db");
-    const { fileURLToPath } = await import("url");
+  const path = await import("path");
+  const { migrate } = await import("drizzle-orm/better-sqlite3/migrator");
+  const { db } = await import("../../../../../drizzle/db");
+  const { fileURLToPath } = await import("url");
 
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
 
-    // This will run migrations on the database, skipping the ones already applied
-    await migrate(db, {
-      migrationsFolder: path.join(
-        __dirname,
-        "..",
-        "..",
-        "..",
-        "..",
-        "..",
-        "drizzle",
-        "migrations",
-      ),
-    });
-    return new Response("OK");
-  } catch (err) {
-    return new Response("Error: " + err);
-  }
+  // This will run migrations on the database, skipping the ones already applied
+  await migrate(db, {
+    migrationsFolder: path.join(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "..",
+      "..",
+      "drizzle",
+      "migrations",
+    ),
+  });
+
+  return new Response("OK");
 }
