63be9aebd4cabf4fd0ab8f2d7eb04949f0ee147c
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-04-05 16:37:07 +0200

    Temporarily log errors

--- src/app/api/admin/migrate/route.ts
+++ src/app/api/admin/migrate/route.ts
@@ -1,25 +1,28 @@
 export async function POST(req: Request) {
-  const path = await import("path");
-  const { migrate } = await import("drizzle-orm/better-sqlite3/migrator");
-  const { db } = await import("../../../../../drizzle/db");
-  const { fileURLToPath } = await import("url");
+  try {
+    const path = await import("path");
+    const { migrate } = await import("drizzle-orm/better-sqlite3/migrator");
+    const { db } = await import("../../../../../drizzle/db");
+    const { fileURLToPath } = await import("url");
 
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
 
-  // This will run migrations on the database, skipping the ones already applied
-  await migrate(db, {
-    migrationsFolder: path.join(
-      __dirname,
-      "..",
-      "..",
-      "..",
-      "..",
-      "..",
-      "drizzle",
-      "migrations",
-    ),
-  });
-
-  return new Response("OK");
+    // This will run migrations on the database, skipping the ones already applied
+    await migrate(db, {
+      migrationsFolder: path.join(
+        __dirname,
+        "..",
+        "..",
+        "..",
+        "..",
+        "..",
+        "drizzle",
+        "migrations",
+      ),
+    });
+    return new Response("OK");
+  } catch (err) {
+    return new Response("Error: " + err);
+  }
 }
