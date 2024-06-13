e999c097b89a16f1620fb5554e3ff07f0af50dd3
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-06-13 12:31:14 +0200

    Add a custom error screen

---
+++ src/app/error.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect } from "react";
+import * as Sentry from "@sentry/nextjs";
+import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
+import { ConvexError } from "convex/values";
+
+export default function ErrorPage({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  useEffect(() => {
+    // Log the error to Sentry
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <div className="bg-red-50 h-full items-center justify-center flex p-4">
+      <div className="bg-white border-t-red-500 rounded-xl shadow-2xl overflow-hidden w-80">
+        <div className="h-3 bg-gradient-to-b from-red-500 to-red-600"></div>
+        <div className="flex gap-2 items-center p-6">
+          <ExclamationCircleIcon className="h-6 w-6 text-red-500" />
+          <div className="flex-1 text-gray-900">
+            {error instanceof ConvexError ? error.data : "nonconvex"}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
