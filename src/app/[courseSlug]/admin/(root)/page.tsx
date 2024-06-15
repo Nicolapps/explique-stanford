29cb7f233532047b5d42bfd206185f10900b8b01
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-06-15 18:04:21 +0200

    Improve the appearance

--- src/app/[courseSlug]/admin/(root)/page.tsx
+++ src/app/[courseSlug]/admin/(root)/page.tsx
@@ -28,51 +28,53 @@ export default function Admin() {
         </Link>
       </Title>
 
-      {weeks?.map((week) => (
-        <div key={week._id}>
-          <h2 className="text-3xl font-medium mt-8 mb-4 flex gap-3 flex-wrap items-baseline">
-            <span>{week.name}</span>
-            <Link
-              href={`/${courseSlug}/admin/weeks/${week._id}`}
-              className="font-medium text-blue-800 flex items-center text-base gap-1"
-            >
-              Edit
-            </Link>
-          </h2>
-          <p className="text-gray-700">
-            <strong className="font-medium text-gray-800">
-              {formatTimestampHumanFormat(week.startDate)}
-            </strong>{" "}
-            to{" "}
-            <strong className="font-medium text-gray-800">
-              {formatTimestampHumanFormat(week.endDate)}
-            </strong>
-          </p>
+      <div className="grid gap-12">
+        {weeks?.map((week) => (
+          <div key={week._id}>
+            <h2 className="text-3xl font-medium flex mb-4 gap-3 flex-wrap items-baseline">
+              <span>{week.name}</span>
+              <Link
+                href={`/${courseSlug}/admin/weeks/${week._id}`}
+                className="font-medium text-blue-800 flex items-center text-base gap-1"
+              >
+                Edit
+              </Link>
+            </h2>
+            <p className="text-gray-700">
+              <strong className="font-medium text-gray-800">
+                {formatTimestampHumanFormat(week.startDate)}
+              </strong>{" "}
+              to{" "}
+              <strong className="font-medium text-gray-800">
+                {formatTimestampHumanFormat(week.endDate)}
+              </strong>
+            </p>
 
-          <div className="mt-4 grid gap-6 md:grid-cols-2">
-            {week.exercises.map((exercise) => (
-              <ExerciseLink
-                href={`/${courseSlug}/admin/exercises/${exercise.id}`}
-                name={exercise.name}
-                image={exercise.image}
-                key={exercise.id}
-              />
-            ))}
+            <div className="mt-4 grid gap-6 md:grid-cols-2">
+              {week.exercises.map((exercise) => (
+                <ExerciseLink
+                  href={`/${courseSlug}/admin/exercises/${exercise.id}`}
+                  name={exercise.name}
+                  image={exercise.image}
+                  key={exercise.id}
+                />
+              ))}
 
-            <Link
-              href={`/${courseSlug}/admin/exercises/new/${week._id}`}
-              className="block rounded-3xl shadow-[inset_0_0_0_2px_#bfdbfe] transition-shadow hover:shadow-[inset_0_0_0_2px_#3b82f6]"
-            >
-              <div className="relative pb-[57.14%]">
-                <div className="absolute inset-0 flex flex-col items-center justify-center text-blue-900 text-xl font-light gap-2">
-                  <PlusIcon className="w-6 h-6" />
-                  <span>New Exercise</span>
+              <Link
+                href={`/${courseSlug}/admin/exercises/new/${week._id}`}
+                className="block rounded-3xl shadow-[inset_0_0_0_2px_#bfdbfe] transition-shadow hover:shadow-[inset_0_0_0_2px_#0084c7]"
+              >
+                <div className="relative pb-[57.14%]">
+                  <div className="absolute inset-0 flex flex-col items-center justify-center text-sky-600 text-xl gap-2">
+                    <PlusIcon className="w-6 h-6" />
+                    <span>New Exercise</span>
+                  </div>
                 </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </>
   );
 }
