ce8a0959fbf1717df217535202ee55d3db6e6d47
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-06-16 18:05:56 +0200

    Add a Modal component

--- src/app/a/[id]/page.tsx
+++ src/app/a/[id]/page.tsx
@@ -9,9 +9,9 @@ import ExplainExercise from "@/components/exercises/ExplainExercise";
 import QuizExercise from "@/components/exercises/QuizExercise";
 import ReadingExercise from "@/components/exercises/ReadingExercise";
 import { Fragment, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
 import Tooltip from "@/components/Tooltip";
 import { Button } from "@/components/Button";
+import { Modal } from "@/components/Modal";
 
 export default function Page({ params }: { params: { id: string } }) {
   const attemptId = params.id as Id<"attempts">;
@@ -138,71 +138,35 @@ function RestartButton({ exerciseId }: { exerciseId: Id<"exercises"> }) {
         </Tooltip>
       </div>
 
-      <Transition appear show={isModalOpen} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-10"
-          onClose={() => setIsModalOpen(false)}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="Restart the exercise?"
+      >
+        <div className="mt-2">
+          <p className="text-sm text-gray-500">
+            If you’re experiencing issues, you can restart the exercise and try
+            again.
+          </p>
+        </div>
+
+        <div className="mt-4 flex gap-2 justify-end">
+          <Button
+            onClick={() => setIsModalOpen(false)}
+            variant="secondary"
+            size="sm"
           >
-            <div className="fixed inset-0 bg-black/25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
-                  >
-                    Restart the exercise?
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      If you’re experiencing issues, you can restart the
-                      exercise and try again.
-                    </p>
-                  </div>
-
-                  <div className="mt-4 flex gap-2 justify-end">
-                    <Button
-                      onClick={() => setIsModalOpen(false)}
-                      variant="secondary"
-                      size="sm"
-                    >
-                      Cancel
-                    </Button>
-                    <Button
-                      href={`/a/new?exerciseId=${exerciseId}`}
-                      variant="danger"
-                      size="sm"
-                    >
-                      Restart
-                    </Button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
+            Cancel
+          </Button>
+          <Button
+            href={`/a/new?exerciseId=${exerciseId}`}
+            variant="danger"
+            size="sm"
+          >
+            Restart
+          </Button>
+        </div>
+      </Modal>
     </>
   );
 }
