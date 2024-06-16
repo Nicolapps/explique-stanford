8bbd05c09f1188ca13ac0c44c013bb5b8fc313fe
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-06-16 15:57:08 +0200

    Improve the admin button layout

--- src/app/[courseSlug]/page.tsx
+++ src/app/[courseSlug]/page.tsx
@@ -27,6 +27,7 @@ import {
   Transition,
 } from "@headlessui/react";
 import { ExerciseLink } from "@/components/ExerciseLink";
+import { TabBar } from "@/components/TabBar";
 
 function Login() {
   const router = useRouter();
@@ -193,24 +194,22 @@ export default function CoursePage() {
 
             <div className="bg-white shadow-[0_-20px_40px_-12px_rgb(0_0_0_/_0.1)] rounded-t-2xl p-8 md:p-14 w-full max-w-2xl mx-auto mt-8">
               <CourseSelector />
-              {user && user.isAdmin && (
-                <div className="mt-2 mb-8 text-center">
-                  <Link
-                    href={`/${courseSlug}/admin`}
-                    className="text-base font-medium px-4 py-2 rounded-lg bg-red-100 cursor-pointer hover:bg-red-200 inline-flex"
-                  >
-                    Admin
-                  </Link>
-                </div>
-              )}
             </div>
           </div>
         </div>
       </div>
       <div className="relative p-6 sm:p-10 flex justify-center shadow-[0_-10px_10px_-3px_rgba(0_0_0_/_0.08)]">
         <div className="max-w-6xl flex-1">
-          {user ? <ProjectGrid /> : <ProjectGridSkeleton />}
+          {user?.isAdmin && (
+            <TabBar
+              items={[
+                { label: "Exercises", href: `/${courseSlug}` },
+                { label: "Admin", href: `/${courseSlug}/admin` },
+              ]}
+            />
+          )}
 
+          {user ? <ProjectGrid /> : <ProjectGridSkeleton />}
           <div className="h-10" />
         </div>
       </div>
---
+++ src/components/TabBar.tsx
@@ -0,0 +1,63 @@
+import clsx from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+function TabBarLink({ href, label }: { href: string; label: React.ReactNode }) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+  return (
+    <Link
+      href={href}
+      className={clsx(
+        "flex items-center justify-center px-6 py-1 relative font-medium text-lg tracking-tight text-slate-700",
+      )}
+      aria-selected={isActive}
+    >
+      <span className="select-none pointer-events-none opacity-0" aria-hidden>
+        {label}
+      </span>
+      <span
+        className={clsx(
+          "absolute flex items-center justify-center inset-0 transition-all",
+          isActive && "font-semibold text-slate-700",
+        )}
+      >
+        {label}
+      </span>
+    </Link>
+  );
+}
+
+export function TabBar({
+  items,
+}: {
+  items: {
+    label: string;
+    href: string;
+  }[];
+}) {
+  const pathName = usePathname();
+  const selectedIndex = items.findIndex((item) => item.href === pathName);
+
+  return (
+    <div className="mb-7 flex justify-center">
+      <nav className="bg-slate-200 p-[0.375rem] rounded-full overflow-hidden">
+        <div className="relative grid grid-flow-col auto-cols-fr">
+          {selectedIndex !== -1 && (
+            <span
+              className="absolute rounded-full h-full bg-white transition-transform shadow-md"
+              role="presentation"
+              style={{
+                width: `calc(100% / ${items.length})`,
+                transform: `translateX(calc(${selectedIndex}00%))`,
+              }}
+            ></span>
+          )}
+          {items.map((item) => (
+            <TabBarLink key={item.href} href={item.href} label={item.label} />
+          ))}
+        </div>
+      </nav>
+    </div>
+  );
+}
