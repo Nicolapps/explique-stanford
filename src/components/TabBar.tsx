6f04fb109849b904816621d430c47e558ddab367
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-06-21 14:38:02 +0200

    Fix the outline style

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -9,7 +9,7 @@ function TabBarLink({ href, label }: { href: string; label: React.ReactNode }) {
     <Link
       href={href}
       className={clsx(
-        "flex items-center justify-center px-6 py-1 relative font-medium text-lg tracking-tight text-slate-700",
+        "flex items-center justify-center px-6 py-1 relative font-medium text-lg tracking-tight text-slate-700 rounded-full focus:ring-4 focus:outline-none",
       )}
       aria-selected={isActive}
     >
