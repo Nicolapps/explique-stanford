f4d7337e7399291dd7d4911803b9c032931e86bd
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-06-22 13:20:59 +0200

    Apply the same layout fixes on the admin page

--- src/app/[courseSlug]/admin/(root)/page.tsx
+++ src/app/[courseSlug]/admin/(root)/page.tsx
@@ -25,18 +25,18 @@ export default function AdminExercisePage() {
       <div className="bg-gradient-to-b from-purple-200 via-indigo-200 to-blue-200 rounded-lg shadow">
         <div className="p-6 sm:p-10 flex justify-center">
           <div className="max-w-6xl flex-1">
-            <div className="bg-white shadow-xl rounded-2xl p-8 md:p-14 w-full max-w-2xl mx-auto">
+            <div className="bg-white shadow-xl rounded-2xl p-2 md:p-14 w-full max-w-2xl mx-auto">
               {course ? (
-                <div className="flex flex-col justify-center text-center items-center h-24 sm:h-32">
+                <div className="flex flex-col justify-center text-center items-center h-28 sm:h-32">
                   <span className="block sm:text-xl font-bold tracking-wider text-gray-500 sm:mb-1">
                     {course.code}
                   </span>
-                  <span className="block text-balance text-3xl sm:text-4xl font-semibold tracking-tight text-gray-800">
+                  <span className="block text-balance text-xl sm:text-3xl md:text-4xl font-semibold tracking-tight leading-tight text-gray-800">
                     {course.name}
                   </span>
                 </div>
               ) : (
-                <div className="w-full mx-auto h-24 sm:h-32 rounded-xl bg-slate-200 animate-pulse"></div>
+                <div className="w-full mx-auto h-28 sm:h-32 rounded-xl bg-slate-200 animate-pulse"></div>
               )}
             </div>
           </div>
